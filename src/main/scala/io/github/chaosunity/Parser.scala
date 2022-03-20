39d3c734148a77d6d6c1853a2d381802e4bdb499
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-03-21 02:13:54 +0800

    cleanup

--- src/main/scala/io/github/chaosunity/Parser.scala
+++ src/main/scala/io/github/chaosunity/Parser.scala
@@ -3,6 +3,9 @@ package io.github.chaosunity
 object Parser:
   type Functor[T] = T => Option[(T, T)]
 
+  private val WHITESPACE_PREDICATE: Char => Boolean =
+    c => c == ' ' || c == '\t' || c == '\r' || c == '\n'
+
   def preceded[T](p1: Functor[T], p2: Functor[T]): Functor[T] =
     (input: T) =>
       p1(input) match
@@ -35,28 +38,30 @@ object Parser:
   def tag(tag: String): Functor[String] =
     (input: String) =>
       if input startsWith tag then
-        Some(input replaceFirst(s"^$tag", ""), tag)
+        Some(input drop tag.length, tag)
       else None
 
   def char(char: Char): Functor[String] =
     (input: String) =>
-      val charStr = char.toString
-      if input startsWith charStr then
-        Some(input drop 1, charStr)
-      else
-        None
+      tag(char.toString)(input)
+
+  def take_until(predicate: Char => Boolean): Functor[String] =
+    (input: String) =>
+      input takeWhile (!predicate(_)) match
+        case result if result.nonEmpty =>
+          Some(input dropWhile (!predicate(_)), result)
+        case _ => None
 
   def multispace0(): Functor[String] =
     (input: String) =>
-      Some(input takeWhile (c => c == ' ' || c == '\t' || c == '\r' || c == '\n'), input)
+      Some(input takeWhile WHITESPACE_PREDICATE, input dropWhile WHITESPACE_PREDICATE)
 
   def multispace1(): Functor[String] =
     (input: String) =>
       multispace0()(input) match
         case Some((split, input)) =>
-          split match {
+          split match
             case split if split.isEmpty => None
             case _ => Some(split, input)
-          }
         case None => None
 
