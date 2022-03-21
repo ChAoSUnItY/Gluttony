a7f2f2949e5072cbf87fb7a53bddb84b245d5e91
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-03-21 08:35:24 +0800

    format and refactor functions

--- src/main/scala/io/github/chaosunity/Parser.scala
+++ src/main/scala/io/github/chaosunity/Parser.scala
@@ -7,43 +7,38 @@ object Parser:
     c => c == ' ' || c == '\t' || c == '\r' || c == '\n'
 
   def preceded[T](p1: Functor[T], p2: Functor[T]): Functor[T] =
-    (input: T) =>
-      p1(input) match
-        case Some((_, result)) => p2(result)
-        case None => None
+    p1(_) match
+      case Some((_, result)) => p2(result)
+      case None              => None
 
   def terminated[T](p1: Functor[T], p2: Functor[T]): Functor[T] =
-    (input: T) =>
-      p1(input) match
-        case Some((remain, result)) =>
-          p2(remain) match
-            case Some((remain, _)) =>
-              Some(remain, result)
-            case None => None
-        case None => None
+    p1(_) match
+      case Some((remain, result)) =>
+        p2(remain) match
+          case Some((remain, _)) =>
+            Some(remain, result)
+          case None => None
+      case None => None
 
   def delimited[T](p1: Functor[T], p2: Functor[T], p3: Functor[T]): Functor[T] =
-    (input: T) =>
-      p1(input) match
-        case Some((remain, _)) =>
-          p2(remain) match
-            case Some((remain, result)) =>
-              p3(remain) match
-                case Some((remain, _)) =>
-                  Some(remain, result)
-                case None => None
-            case None => None
-        case None => None
+    p1(_) match
+      case Some((remain, _)) =>
+        p2(remain) match
+          case Some((remain, result)) =>
+            p3(remain) match
+              case Some((remain, _)) =>
+                Some(remain, result)
+              case None => None
+          case None => None
+      case None => None
 
   def tag(tag: String): Functor[String] =
     (input: String) =>
-      if input startsWith tag then
-        Some(input drop tag.length, tag)
+      if input startsWith tag then Some(input drop tag.length, tag)
       else None
 
   def char(char: Char): Functor[String] =
-    (input: String) =>
-      tag(char.toString)(input)
+    tag(char.toString)(_)
 
   def take_until(predicate: Char => Boolean): Functor[String] =
     (input: String) =>
@@ -52,16 +47,21 @@ object Parser:
           Some(input dropWhile (!predicate(_)), result)
         case _ => None
 
-  def multispace0(): Functor[String] =
+  private def predicate0(predicate: Char => Boolean): Functor[String] =
     (input: String) =>
-      Some(input takeWhile WHITESPACE_PREDICATE, input dropWhile WHITESPACE_PREDICATE)
+      Some(input takeWhile predicate, input dropWhile predicate)
+
+  private def predicate1(predicateFunc: Functor[String]): Functor[String] =
+    predicateFunc(_) match
+      case Some((split, input)) =>
+        split match
+          case split if !split.isEmpty =>
+            Some(split, input)
+          case _ => None
+      case None => None
+
+  def multispace0(): Functor[String] =
+    predicate0(WHITESPACE_PREDICATE)
 
   def multispace1(): Functor[String] =
-    (input: String) =>
-      multispace0()(input) match
-        case Some((split, input)) =>
-          split match
-            case split if split.isEmpty => None
-            case _ => Some(split, input)
-        case None => None
-
+    predicate1(multispace0())
